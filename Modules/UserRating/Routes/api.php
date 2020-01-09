edfb4ba4b8c93f1edaff1209de3c5a8644b64da5
Toto Prasetyo <toto@technopartner.id>
2020-01-09 11:02:45 +0700

    fix middleware

--- Modules/UserRating/Routes/api.php
+++ Modules/UserRating/Routes/api.php
@@ -18,7 +18,7 @@ Route::group(['middleware' => ['auth:api', 'log_activities', 'user_agent'], 'pre
     Route::post('get-detail', 'ApiUserRatingController@getDetail');
 });
 
-Route::group(['middleware' => ['auth:api', 'log_activities', 'user_agent'], 'prefix' => 'user-rating'], function () {
+Route::group(['middleware' => ['auth:api-be', 'log_activities', 'user_agent'], 'prefix' => 'user-rating'], function () {
     Route::post('/', ['middleware' => 'feature_control:197', 'uses' => 'ApiUserRatingController@index']);
     Route::post('detail', ['middleware' => 'feature_control:197', 'uses' => 'ApiUserRatingController@show']);
     Route::post('delete', ['middleware' => 'feature_control:198', 'uses' => 'ApiUserRatingController@destroy']);
