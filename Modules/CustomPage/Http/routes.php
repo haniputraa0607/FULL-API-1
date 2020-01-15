83b4108864c238073cc13a8e34fc120b21eae131
Dina <dina@technopartner.id>
2020-01-15 19:54:46 +0700

    update auth with scope

--- Modules/CustomPage/Http/routes.php
+++ Modules/CustomPage/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => ['auth:api-be', 'log_activities', 'user_agent'], 'prefix' => 'api/custom-page', 'namespace' => 'Modules\CustomPage\Http\Controllers'], function () {
+Route::group(['middleware' => ['auth:api', 'log_activities', 'user_agent', 'scopes:ap'], 'prefix' => 'api/custom-page', 'namespace' => 'Modules\CustomPage\Http\Controllers'], function () {
     Route::get('be/list', ['middleware' => 'feature_control:149', 'uses' =>'ApiCustomPageController@index']);
     Route::post('create', ['middleware' => 'feature_control:150', 'uses' =>'ApiCustomPageController@store']);
     Route::post('detail', ['middleware' => 'feature_control:153', 'uses' =>'ApiCustomPageController@show']);
@@ -8,7 +8,7 @@ Route::group(['middleware' => ['auth:api-be', 'log_activities', 'user_agent'], '
     Route::post('delete', ['middleware' => 'feature_control:152', 'uses' =>'ApiCustomPageController@destroy']);
 });
 
-Route::group(['middleware' => ['auth:api', 'log_activities'], 'prefix' => 'api/custom-page', 'namespace' => 'Modules\CustomPage\Http\Controllers'], function () {
+Route::group(['middleware' => ['auth:api', 'log_activities', 'scopes:*'], 'prefix' => 'api/custom-page', 'namespace' => 'Modules\CustomPage\Http\Controllers'], function () {
     Route::get('list_custom_page', ['middleware' => 'feature_control:149', 'uses' =>'ApiCustomPageController@listCustomPage']);
     Route::get('list', 'ApiCustomPageController@index');
     Route::get('webview/{id}', 'ApiCustomPageController@webviewCustomPage');														 
