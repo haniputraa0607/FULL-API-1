048a4e02a3fc9eff8a0285d4a7e5a4e14f7c2955
Toto Prasetyo <toto@technopartner.id>
2020-01-31 11:37:45 +0700

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -26,5 +26,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(SettingJobsCelebrateSeeder::class);
         $this->call(ProvinceCustomsTableSeeder::class);
+        $this->call(PromoCampaignsTableSeeder::class);
     }
 }
