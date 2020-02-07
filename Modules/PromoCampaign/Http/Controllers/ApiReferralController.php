31b2dce40ec8067cb922adc40279b1bd4a26b3ed
Yogie Setya Nugraha <yogiesetyanugraha@gmail.com>
2020-02-07 16:44:16 +0700

    Update flow webview referral

--- Modules/PromoCampaign/Http/Controllers/ApiReferralController.php
+++ Modules/PromoCampaign/Http/Controllers/ApiReferralController.php
@@ -10,6 +10,7 @@ use \App\Lib\MyHelper;
 
 use \Modules\PromoCampaign\Entities\PromoCampaignReferral;
 use \Modules\PromoCampaign\Entities\UserReferralCode;
+use App\Http\Models\Setting;
 
 class ApiReferralController extends Controller
 {
@@ -18,6 +19,34 @@ class ApiReferralController extends Controller
      * @return Response
      */
     public function index(Request $request)
+    {
+        $user = $request->user();
+
+        $setting = Setting::where('key', 'referral_messages')->first();
+        if (!$setting) {
+            $setting = [
+                'key'           => 'referral_messages',
+                'value_text'    => 'Get %value% discount on your first purchase. By using the %code% promo code'
+            ];
+            $setting = Setting::create($setting);
+        }
+
+        $referral = UserReferralCode::with(['promo_code', 'promo_code.promo_campaign_referral'])->where('id_user', $user->id)->get()->first();
+
+        $value = implode('', [$referral->promo_code->promo_campaign_referral->referred_promo_value, $retVal = ($referral->promo_code->promo_campaign_referral->referred_promo_unit == 'Percent') ? '%' : ' point']);
+        
+        $data = [
+            'status'    => 'success',
+            'result'    => [
+                'messages'      => str_replace(['%value%', '%code%'], [$value, $referral->promo_code->promo_code], $setting->value_text),
+                'url_webview'   => env('API_URL') . 'api/referral/webview'
+            ]
+        ];
+
+        return response()->json($data);
+    }
+
+    public function webview(Request $request)
     {
         $user = $request->user();
         $referral = UserReferralCode::with(['promo_code', 'promo_code.promo_campaign_referral'])->where('id_user', $user->id)->get()->first();
