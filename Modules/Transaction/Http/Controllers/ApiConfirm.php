053859746e57c3633cdc652487b9d207835d7470
Toto Prasetyo <toto@technopartner.id>
2020-04-01 09:18:51 +0700

    fix payment id

--- Modules/Transaction/Http/Controllers/ApiConfirm.php
+++ Modules/Transaction/Http/Controllers/ApiConfirm.php
@@ -371,7 +371,8 @@ class ApiConfirm extends Controller
                 'result'    => [
                     'url'  => env('API_URL').'api/ipay88/pay?'.http_build_query([
                         'type' => 'trx',
-                        'id_reference' => $check['id_transaction']
+                        'id_reference' => $check['id_transaction'],
+                        'payment_id' => $request->payment_id?:''
                     ])
                 ]
             ];
