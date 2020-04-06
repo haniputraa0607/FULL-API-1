fb460eba0d0b94f0e0946f5e4b171473e155b441
Yogie Setya Nugraha <yogiesetyanugraha@gmail.com>
2020-04-06 08:41:06 +0700

    Update Voucher

--- Modules/Deals/Http/Controllers/ApiDealsVoucherWebviewController.php
+++ Modules/Deals/Http/Controllers/ApiDealsVoucherWebviewController.php
@@ -72,7 +72,7 @@ class ApiDealsVoucherWebviewController extends Controller
     }
 
     
-    public function detailVoucher(Request $request, $id_deals_user)
+    public function detailVoucher(Request $request)
     {
         $bearer = $request->header('Authorization');
 
@@ -80,7 +80,7 @@ class ApiDealsVoucherWebviewController extends Controller
             return abort(404);
         }
 
-        $post['id_deals_user'] = $id_deals_user;
+        $post['id_deals_user'] = $request->id_deals_user;
         $post['used'] = 0;
 
         $action = MyHelper::postCURLWithBearer('api/voucher/me?log_save=0', $post, $bearer);
@@ -114,27 +114,40 @@ class ApiDealsVoucherWebviewController extends Controller
                 'id_deals_user'         => $data['id_deals_user'],
                 'voucher_expired'       => date('d F Y', strtotime($data['voucher_expired_at'])),
                 'is_used'               => $data['is_used'],
+                'btn_used'              => 'Use Later',
                 'is_online'             => $data['is_online'],
+                'btn_online'            => 'Use Voucher',
                 'is_offline'            => $data['is_offline'],
+                'btn_offline'           => 'Redeem to Cashier',
+                'header_online_voucher' => 'Online Transaction',
+                'title_online_voucher'  => 'Apply promo on this app',
+                'header_offline_voucher'=> 'Offline Transaction',
+                'title_offline_voucher' => 'Redeem directly at Cashier',
+                'button_text'           => $data['button_text'],
+                'popup_message'         => $data['popup_message'],
             ];
 
             $i = 0;
             foreach ($data['deal_voucher']['deal']['deals_content'] as $keyContent => $valueContent) {
-                $result['deals_content'][$keyContent]['title'] = $valueContent['title'];
-                foreach ($valueContent['deals_content_details'] as $key => $value) {
-                    $result['deals_content'][$keyContent]['detail'][$key]['content'] = $value['content'];
+                if (!empty($valueContent['deals_content_details'])) {
+                    $result['deals_content'][$keyContent]['title'] = $valueContent['title'];
+                    foreach ($valueContent['deals_content_details'] as $key => $value) {
+                        $content[$key] = '<ol style="line-height: 10px;">' . $value['content'] . '</ol>';
+                    }
+                    $result['deals_content'][$keyContent]['detail'] = '<ul>'.implode('', $content).'</ul>';
+                    $i++;
                 }
-                $i++;
             }
 
             $result['deals_content'][$i]['is_outlet'] = 1;
             $result['deals_content'][$i]['title'] = 'Available at';
             foreach ($data['deal_voucher']['deal']['outlet_by_city'] as $keyCity => $valueCity) {
                 if (isset($valueCity['city_name'])) {
-                    $result['deals_content'][$i]['detail'][$keyCity]['city'] = strtoupper($valueCity['city_name']);
-                    foreach ($valueCity['outlet'] as $keyOutlet => $valueOutlet) {
-                        $result['deals_content'][$i]['detail'][$keyCity]['outlet'][$keyOutlet] = strtoupper($valueOutlet['outlet_name']);
+                    foreach($valueCity['outlet'] as $keyOutlet => $valueOutlet) {
+                        $outlet[$keyOutlet] = '<li style="line-height: 12px;">' . $valueOutlet['outlet_name'] . '</li>'; 
                     }
+                    $city[$keyCity] = strtoupper($valueCity['city_name']) . '<br>' . implode('', $outlet);
+                    $result['deals_content'][$i]['detail'] = '<ul>'.implode('', $city).'</ul>';
                 }
             }
 
