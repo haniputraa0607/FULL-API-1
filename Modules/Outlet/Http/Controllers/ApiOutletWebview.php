8a0ab125e2d47875123d357f20aa5d093c194c0f
Yogie Setya Nugraha <yogiesetyanugraha@gmail.com>
2020-04-08 15:37:44 +0700

    Update Webview to Native
    - Voucher
    - Outlet

--- Modules/Outlet/Http/Controllers/ApiOutletWebview.php
+++ Modules/Outlet/Http/Controllers/ApiOutletWebview.php
@@ -41,25 +41,29 @@ class ApiOutletWebview extends Controller
         // ], $bearer);
 
         
-        $outlet = Outlet::with(['outlet_schedules'])
+        $outlet = Outlet::with(['today', 'outlet_schedules'])
         ->where('id_outlet', $request->id_outlet)->get()->toArray()[0];
 
         $outlet['distance']=number_format((float)$this->distance($request->latitude, $request->longitude, $outlet['outlet_latitude'], $outlet['outlet_longitude'], "K"), 2, '.', '').' km';
-
+        
         foreach ($outlet['outlet_schedules'] as $key => $value) {
             if (date('l') == $value['day']) {
                 $outlet['outlet_schedules'][$key] = [
                     'is_today'  => 1,
                     'day'       => substr($value['day'], 0, 3),
-                    'time'      => $value['open'] . ' - ' . $value['close']
+                    'time'      => $value['open'] . ' - ' . $value['close'],
+                    'is_closed' => $value['is_closed']
                 ];
             } else {
                 $outlet['outlet_schedules'][$key] = [
                     'day'       => substr($value['day'], 0, 3),
-                    'time'      => $value['open'] . ' - ' . $value['close']
+                    'time'      => $value['open'] . ' - ' . $value['close'],
+                    'is_closed' => $value['is_closed']
                 ];
             }
         }
+        $outlet['is_closed'] = $outlet['today']['is_closed'];
+        unset($outlet['today']);
         unset($outlet['url']);
         unset($outlet['detail']);
         unset($outlet['created_at']);
