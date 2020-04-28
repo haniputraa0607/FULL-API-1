c317882eec841e5b06bde50cc567c679d38fe131
Toto Prasetyo <toto@technopartner.id>
2020-04-28 21:20:17 +0700

    fix error

--- resources/views/emails/email_header.blade.php
+++ resources/views/emails/email_header.blade.php
@@ -433,7 +433,7 @@
                 <tr>
                     <td style="border-collapse:collapse;border-spacing:0;color:#999;font-family:'Source Sans Pro',sans-serif;line-height:1.5;margin:0;padding:0" width="15" height="100"></td>
                     <td id="logoheader_center" style="border-collapse:collapse;border-spacing:0;color:#000;font-family:'Arial',sans-serif;line-height:1.5;margin:0;padding:0;text-align:center" height="100">
-                        <img class="CToWUd" id="detail_logo_center" @if(stristr($setting['email_logo'], 'http')) src="{{$setting['email_logo']}}" @else src="{{env('AWS_URL')}}{{$setting['email_logo']}}" @endif style="border:0 none;line-height:100%;outline:none;text-decoration:none;height: 100px;margin-bottom: -60px;width: 150px;" alt="" height="50px">
+                        <img class="CToWUd" id="detail_logo_center" @if(stristr($setting['email_logo']??'', 'http')) src="{{$setting['email_logo']}}" @else src="{{env('AWS_URL')}}{{$setting['email_logo']??''}}" @endif style="border:0 none;line-height:100%;outline:none;text-decoration:none;height: 100px;margin-bottom: -60px;width: 150px;" alt="" height="50px">
                     </td>
                     <td style="border-collapse:collapse;border-spacing:0;color:#999;font-family:'Source Sans Pro',sans-serif;line-height:1.5;margin:0;padding:0" width="15" height="100"></td>
                 </tr>
