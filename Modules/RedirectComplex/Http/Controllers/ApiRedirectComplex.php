7f53bb007d910ae17eee7ef6a54b73e9de11f6b8
damar <damar@technopartner.id>
2020-08-11 14:41:08 +0700

    add api redirect complex get detail

--- Modules/RedirectComplex/Http/Controllers/ApiRedirectComplex.php
+++ Modules/RedirectComplex/Http/Controllers/ApiRedirectComplex.php
@@ -9,6 +9,8 @@ use Modules\RedirectComplex\Entities\RedirectComplexReference;
 use Modules\RedirectComplex\Entities\RedirectComplexProduct;
 use Modules\RedirectComplex\Entities\RedirectComplexOutlet;
 
+use Modules\PromoCampaign\Entities\PromoCampaignPromoCode;
+
 use Modules\RedirectComplex\Http\Requests\CreateRequest;
 use Modules\RedirectComplex\Http\Requests\EditRequest;
 use Modules\RedirectComplex\Http\Requests\UpdateRequest;
@@ -19,6 +21,8 @@ use Modules\ProductVariant\Entities\ProductGroup;
 use Modules\ProductVariant\Entities\ProductVariant;
 use App\Http\Models\Outlet;
 use App\Http\Models\Setting;
+use App\Http\Models\Product;
+use Modules\Brand\Entities\Brand;
 use App\Lib\MyHelper;
 use DB;
 
@@ -28,7 +32,9 @@ class ApiRedirectComplex extends Controller
 	function __construct()
 	{
         date_default_timezone_set('Asia/Jakarta');
-		$this->outlet = "Modules\Outlet\Http\Controllers\ApiOutletController";
+		$this->outlet 				= "Modules\Outlet\Http\Controllers\ApiOutletController";
+		$this->online_transaction   = "Modules\Transaction\Http\Controllers\ApiOnlineTransaction";
+		$this->api_promo   			= "Modules\PromoCampaign\Http\Controllers\ApiPromo";
 	}
 
     /**
@@ -220,20 +226,21 @@ class ApiRedirectComplex extends Controller
     {
     	$post = $request->all();
     	$data = [
-    		'id_outlet' => null,
-    		'data' 		=> [],
-    		'promo' 	=> []
+    		'id_outlet' => null
     	];
+
     	$reference 	= RedirectComplexReference::where('id_redirect_complex_reference', $request->id_reference)
 					->with([
 	    				'outlets' => function($q) {
 	    					$q->select('outlets.id_outlet', 'outlet_code', 'outlet_name');
 	    				},
 	    				'products' => function($q) {
-	    					$q->select('products.id_product', 'product_code', 'product_name');
+	    					$q->with(['brands', 'product_variants']);
+	    					// $q->select('products.id_product', 'product_code', 'product_name');
 	    				}
 	    			])
 					->first();
+
 		if (!$reference) {
 			return ['status' => 'fail', 'messages' => ['Reference not found']];
 		}
@@ -244,19 +251,24 @@ class ApiRedirectComplex extends Controller
     	if ($outlet['status'] == 'fail') {
 			return ['status' => 'fail', 'messages' => ['Outlet not found']];
 		}else{
-			$data['id_outlet'] = $outlet['data'];
+			$data['id_outlet'] = $outlet['id_outlet'];
 		}
 
-		// get product data
-		$product = $this->getProduct($reference, $data['id_outlet']);
-		if ($product['status'] == 'fail') {
-			return ['status' => 'fail', 'messages' => ['Product not found']];
-		}else{
-			$data['data'] = $product['data'];
-		}
+		// get product
+		$data['item'] = $this->getProduct($reference->products);
 
 		// get promo
-    	return $data;
+		$data['promo_code'] = $this->getPromo($reference->promo_reference);
+		
+		$custom_request = new \Modules\Transaction\Http\Requests\CheckTransaction;
+		$custom_request = $custom_request
+						->setJson(new \Symfony\Component\HttpFoundation\ParameterBag($data))
+						->merge($data)
+						->setUserResolver(function () use ($request) {
+							return $request->user();
+						});
+
+		return app($this->online_transaction)->checkTransaction($custom_request);
     }
 
     function getOutlet($latitude, $longitude, $outlet_list=[], $outlet_type=null) {
@@ -306,48 +318,52 @@ class ApiRedirectComplex extends Controller
 	    		$outlet_list = array_column($outlet_list->toArray(), 'id_outlet');
 	    		foreach ($outlet as $key => $value) {
 	    			if (in_array($value['id_outlet'], $outlet_list)) {
-	    				$outlet = $value['id_outlet'];
+	    				$id_outlet = $value['id_outlet'];
 	    				break;
 	    			}
 	    		}
 	    	}
 	    	else {
-	    		$outlet = $outlet[0]['id_outlet'];
+	    		$id_outlet = $outlet[0]['id_outlet'];
 	    	}
         }
 
-        return ['status' => 'success', 'data' => $outlet];
+        return ['status' => 'success', 'id_outlet' => $id_outlet];
     }
 
-    function getProduct($reference, $id_outlet){
-    	$use_product_variant = \App\Http\Models\Configs::where('id_config',94)->pluck('is_active')->first();
-        $nf 		= 'float';
+    function getProduct($products)
+    {
+    	$data =[];
+    	$brand = Brand::select('id_brand')->first();
+    	foreach ($products as $value) {
+    		$temp = [
+				"id_product_group" => $value['id_product_group'],
+		    	"bonus"		=> 0,
+				"id_brand"	=> $value['brands'][0]['id_brand']??$brand['id_brand'],
+				"modifiers"	=> [],
+				"note"		=> "",
+				"qty"		=> $value['pivot']['qty'],
+				"variants"	=> []
+			];
 
-        $data = $reference->products;
-        $product_group = [];
-        if($use_product_variant){
-        	foreach ($data as $key => $value) {
-				$product_group[$key] = ProductGroup::select(\DB::raw('product_groups.id_product_group,product_group_name,product_group_code,product_group_description,product_group_photo,product_prices.product_price as price'))
-					->join('products','products.id_product_group','=','product_groups.id_product_group')
-					->where('products.id_product',$value->id_product)
-					->join('product_prices','products.id_product','=','product_prices.id_product')
-					->where('product_prices.id_outlet',$id_outlet)
-					->groupBy('products.id_product')->first()->toArray();
-				$product_group[$key]['variants'] = ProductVariant::select('product_variants.id_product_variant','product_variants.product_variant_name','product_variants.product_variant_code')
-					->join('product_product_variants','product_product_variants.id_product_variant','product_variants.id_product_variant')
-					->join('product_variants as parent','parent.id_product_variant', '=', 'product_variants.parent')
-					->where('product_product_variants.id_product',$value->id_product)
-					->orderBy('parent.product_variant_position')
-					->get()->toArray();
-				unset($product_group['products']);
-        	}
-		}
+			foreach ($value['product_variants'] as $value2) {
+				$temp['variants'][] = $value2['id_product_variant'];
+			}
+			$data[] = $temp;
+    	}
+    	return $data;
 
-		if (empty($product_group)) {
-			return ['status' => 'fail', 'messages' => ['Product not found']];			
-		}
-		else {
-			return ['status' => 'success', 'data' => $product_group];
-		}
+    }
+
+    function getPromo($promo_reference)
+    {
+    	$data = null;
+    	if ($promo_reference) {
+    		$promo = PromoCampaignPromoCode::where('id_promo_campaign', $promo_reference)->first();
+    		$data = $promo['promo_code'];
+    	}
+
+    	return $data;
     }
 }
+	
