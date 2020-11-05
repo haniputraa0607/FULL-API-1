f53db70974c8fddead9a51662846991a7e0cd0bb
Toto Prasetyo <toto@technopartner.co.id>
2020-11-05 17:40:32 +0700

    fix log ipay and log shopeepay

--- Modules/IPay88/Entities/LogIpay88.php
+++ Modules/IPay88/Entities/LogIpay88.php
@@ -18,4 +18,18 @@ class LogIpay88 extends Model
     	'response',
     	'response_status_code'
     ];
+
+    public static function __callStatic($method, $parameters)
+    {
+        if ($method == 'create' && count($parameters) == 1) {
+            if (env('DISABLE_LOG')) {
+                return optional(null);
+            }
+
+           return parent::create($parameters[0], true);
+        } elseif ($method == 'create') {
+            return (new static)->$method($parameters[0]);
+        }
+        return (new static)->$method(...$parameters);
+    }
 }
