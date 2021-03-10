4abb2181a5d5128fba5630292c715b49842d22ff
tpid-hani <haniyah.imtiyaz@technopartner.id>
2021-03-10 14:40:36 +0700

    add callback for gopay midtrans

--- Modules/Deals/Http/Controllers/ApiDealsClaimPay.php
+++ Modules/Deals/Http/Controllers/ApiDealsClaimPay.php
@@ -534,7 +534,7 @@ class ApiDealsClaimPay extends Controller
             $data['gross_amount'] = $grossAmount;
         }
 
-        $tembakMitrans = Midtrans::token($data['order_id'], $data['gross_amount']);
+        $tembakMitrans = Midtrans::token($data['order_id'], $data['gross_amount'], null, null, null, 'deals', $voucher->id_deals_user);
 
         // print_r($tembakMitrans); exit();
 
